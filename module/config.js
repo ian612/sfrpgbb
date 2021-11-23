226b8257e6677dfc2be1c9f37c5afb019ae2309d
ian612 <34078802+ian612@users.noreply.github.com>
2021-11-23 09:27:25 -0500

    add localization and first item sheer "weapon"

---
+++ module/config.js
@@ -0,0 +1,22 @@
+export const sfrpgbb = {};
+
+sfrpgbb.attackTypes = {
+    none: "",
+    meleeBasic: "sfrpgbb.attack.meleeBasic",
+    meleeAdvanced: "sfrpgbb.attack.meleeAdvanced",
+    smallArms: "sfrpgbb.attack.smallArms",
+    longArms: "sfrpgbb.attack.longArms",
+    grenades: "sfrpgbb.attack.grenades"
+}
+
+sfrpgbb.weaponSpecials = {
+    none: "",
+    blast: "sfrpgbb.weapon.blast",
+    boost: "sfrpgbb.weapon.boost"
+}
+
+sfrpgbb.weaponCriticals = {
+    none: "",
+    knockdown: "sfrpgbb.weapon.knockdown",
+    stagger: "sfrpgbb.weapon.stagger"
+}
