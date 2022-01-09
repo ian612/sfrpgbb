c25429e51aa698ebd0d34d896fb64d81582cfd49
ian612 <34078802+ian612@users.noreply.github.com>
2022-01-09 18:35:19 -0500

    Updated some UI stuff

--- module/sheets/sfrpgbbActorSheet.js
+++ module/sheets/sfrpgbbActorSheet.js
@@ -26,26 +26,26 @@ export default class sfrpgbbActorSheet extends ActorSheet {
 
     /** @override */
     activateListeners(html) {
-    super.activateListeners(html);
-  
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
-  
-    // Add Inventory Item
-    html.find('.item-create').click(this.actor.createEmbeddedDocuments("Item"));
-  
-    // Update Inventory Item
-    html.find('.item-edit').click(ev => {
-      const li = $(ev.currentTarget).parents(".item");
-      const item = this.actor.items.get(li.data("itemId"));
-      item.sheet.render(true);
-    });
-  
-    // Delete Inventory Item
-    html.find('.item-delete').click(ev => {
-      const li = $(ev.currentTarget).parents(".item");
-      this.actor.deleteEmbeddedDocuments("Item", [li.data("itemId")]);
-      li.slideUp(200, () => this.render(false));
-    });
-  }
+        super.activateListeners(html);
+        
+        // Everything below here is only needed if the sheet is editable
+        if (!this.options.editable) return;
+        
+        // Add Inventory Item
+        // html.find('.item-create').click(ev => this._onItemCreate(ev));;
+
+        // Update Inventory Item
+        html.find('.item-edit').click(ev => {
+          const li = $(ev.currentTarget).parents(".item");
+          const item = this.actor.items.get(li.data("itemId"));
+          item.sheet.render(true);
+        });
+        
+        // Delete Inventory Item
+        html.find('.item-delete').click(ev => {
+          const li = $(ev.currentTarget).parents(".item");
+          this.actor.deleteEmbeddedDocuments("Item", [li.data("itemId")]);
+          li.slideUp(200, () => this.render(false));
+        });
+    }
 }
